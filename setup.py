08e42329fde745d133208cdfe902a7b1f8f8be4c
Burak <burakcivelekk61@gmail.com>
2025-07-16 17:37:15 +0300

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pptxtoimages",
-    version="0.1.8",
+    version="0.1.9",
     packages=find_packages(),
     install_requires=[
         "pdf2image",
